dd614e28184a62ef166f06450bd4cbf7a844aa67
arbaj09 <69784621+arbaj09@users.noreply.github.com>
2020-08-17 12:03:33 +0530

    added new code

---
+++ faulty calculator.py
@@ -0,0 +1,39 @@
+op=input("enter the oprator")
+frist_number=int(input("enetr the frist number"))
+second_number=int(input("enetr the second number"))
+if op=="+":
+    if( frist_number==56 and second_number ==9):
+        print(143)
+
+    else:
+            print(frist_number+second_number)
+            (frist_number+second_number)
+
+if op=="-":
+    if( frist_number==2 and second_number ==2):
+        print(4)
+
+    else:
+            print(frist_number-second_number)
+            (frist_number-second_number)
+if op=="*":
+    if( frist_number==17 and second_number ==9):
+        print(4)
+
+    else:
+            print(frist_number*second_number)
+            (frist_number*second_number)
+
+if op=="/":
+    if( frist_number==2 and second_number ==2):
+        print(8)
+
+    else:
+            print(frist_number/second_number)
+            (frist_number/second_number)
+
+
+
+
+
+
